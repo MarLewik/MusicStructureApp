9bb6cf6d2097c5a9fee1f02ad5cc9c720ca01940
Marcin Lewandowski <marlew.fb@o2.pl>
2018-06-21 07:02:18 +0200

    mics.xml added

--- app/src/main/java/com/example/marlew/musicstructureapp/Song.java
+++ app/src/main/java/com/example/marlew/musicstructureapp/Song.java
@@ -1,4 +1,38 @@
 package com.example.marlew.musicstructureapp;
 
 public class Song {
+
+
+    /** Default song name */
+    private String songName;
+
+    /** Miwok translation for the word */
+    private String artistName;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Song(String sName, String aName) {
+        songName = sName;
+        artistName = aName;
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
 }
