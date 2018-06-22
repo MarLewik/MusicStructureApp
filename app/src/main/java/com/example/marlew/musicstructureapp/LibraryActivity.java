b617c61667b8b88f429772da16f3842130a422a7
Marcin Lewandowski <marlew.fb@o2.pl>
2018-06-22 20:52:14 +0200

    custom data class and custom adapter implementation after review

--- app/src/main/java/com/example/marlew/musicstructureapp/LibraryActivity.java
+++ app/src/main/java/com/example/marlew/musicstructureapp/LibraryActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -12,6 +15,14 @@ public class LibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+
+        ArrayList<Song> songs = new ArrayList<>();
+
+        //Table of 400 songs
+        for (int i=1; i<20; i++)
+            for (int j=1; j<20; j++)
+                songs.add(new Song("song"+i, "artist"+j));
+
 
         // Find the View that shows the play category
         TextView play = (TextView) findViewById(R.id.play);
@@ -39,5 +50,9 @@ public class LibraryActivity extends AppCompatActivity {
                 startActivity(mainIntent);
             }
         });
+
+        SongAdapter adapter = new SongAdapter(this, songs);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/marlew/musicstructureapp/SongAdapter.java
+++ app/src/main/java/com/example/marlew/musicstructureapp/SongAdapter.java
@@ -1,4 +1,53 @@
 package com.example.marlew.musicstructureapp;
 
-public class SongAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class SongAdapter extends ArrayAdapter<Song> {
+
+
+    /**
+     * Create a new {@link SongAdapter} object.
+     *
+     * @param context is the current context (i.e. Activity) that the adapter is being created in.
+     * @param songs is the list of {@link Song}s to be displayed.
+     */
+    public SongAdapter(Context context, ArrayList<Song> songs) {
+        super(context, 0, songs);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Check if an existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link Song} object located at this position in the list
+        Song currentWord = getItem(position);
+
+        // Find the TextView in the list_item.xml layout with the ID songname_text_view.
+        TextView songTextView = (TextView) listItemView.findViewById(R.id.songname_text_view);
+        // Get the songname the currentSong object and set this text on
+        // the Song TextView.
+        songTextView.setText(currentWord.getSongName());
+
+        // Find the TextView in the list_item.xml layout with the ID artistname_text_view.
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.artistname_text_view);
+        // Get the artistname from the currentSong object and set this text on
+        // the artistname TextView.
+        defaultTextView.setText(currentWord.getArtistName());
+
+        // Return the whole list item layout (containing 2 TextViews) so that it can be shown in
+        // the ListView.
+        return listItemView;
+    }
 }
